98901db64bb41186aa618fae5a7c74ac12b79c1a
Liad <liad.magen@gmail.com>
2019-01-04 12:35:19 +0100

    added debugging info and validations

--- kce/api.py
+++ kce/api.py
@@ -19,19 +19,22 @@ def get_latest(req, resp, *, subject):
 class MessageService:
 
     def on_post(self, req, resp):
-        self.fromaddr = req.params['email']
-        self.fromname = req.params['name']
-        self.msg = req.params['message']
+        print(req.params)
         
-        if self.fromaddr and self.fromname and self.msg:
-            self.sendMessage()
-            resp.text = "OK"
-            return
+        if set(['email', 'name', 'message']).issubset(req.params):
+            self.fromaddr = req.params['email']
+            self.fromname = req.params['name']
+            self.msg = req.params['message']
+        
+            if self.fromaddr and self.fromname and self.msg:
+                self.sendMessage()
+                resp.text = "OK"
+                return
 
         resp.text = "..."
 
     def sendMessage(self):
-        self.msg = '''New message from {name} at Keep-Current site: 
+        self.msg = '''<h1> New message from {name} at Keep-Current site: </h1> 
         {msg}'''.format(name=self.fromname, msg=self.msg)
 
         sendMail(self.fromaddr, self.msg)
